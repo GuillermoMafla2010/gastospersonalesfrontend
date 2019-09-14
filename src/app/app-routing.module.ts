eb7b4e15ac24166ea11e0eb2fe82e2cfaa56fa68
GuillermoMafla <cjativa12@hotmail.com>
2019-09-14 12:05:56 -0500

    ingresos-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,12 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  { path: 'usuarios', loadChildren: './componentes/usuarios/usuarios.module#UsuariosPageModule' }
+  { path: 'usuarios', loadChildren: './componentes/usuarios/usuarios.module#UsuariosPageModule' },
+  { path: 'ingresos', loadChildren: './componentes/ingresos/ingresos.module#IngresosPageModule' },
+  { path: 'crear-ingresos', loadChildren: './componentes/ingresos/crear-ingresos/crear-ingresos.module#CrearIngresosPageModule' },
+  { path: 'ver-ingresos', loadChildren: './componentes/ingresos/ver-ingresos/ver-ingresos.module#VerIngresosPageModule' },
+  { path: 'ver-ingresos-fecha', loadChildren: './componentes/ingresos/ver-ingresos-fecha/ver-ingresos-fecha.module#VerIngresosFechaPageModule' },
+  { path: 'editar-ingreso', loadChildren: './componentes/ingresos/editar-ingreso/editar-ingreso.module#EditarIngresoPageModule' },
 ];
 @NgModule({
   imports: [
